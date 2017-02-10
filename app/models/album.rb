567f65243d784a747565c75497d0fce788974932
Nidhi <maveric.nidhi@gmail.com>
2017-02-10 22:31:28 +0530

    4) Album model, recent scope

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,4 +5,8 @@ class Album < ActiveRecord::Base
   validates :name, presence: true, length: { in: 2..50 }
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
+
+  #Scopes
+  scope :recent, -> (n) { where("released_on <= ? and released_on >= ?", Date.today, Date.today-6.months).order("released_on DESC").limit(n) }
+
 end
